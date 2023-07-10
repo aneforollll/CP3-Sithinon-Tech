fe1d0d209d5a0a73044ca1b3471de1f155490155
aneforollll <sithinon2006@gmail.com>
2023-07-10 21:53:58 +0700

    Ex4_Sithinon

---
+++ Ex4_Sithinon.py
@@ -0,0 +1,8 @@
+FoundationEnglishScore = float(input("Put your Foundation English Score here :"))
+GeneralBusinessScore = float(input("Put your General Business Score here :"))
+IntroductiontoComputerSystemsScore = float(input("Put your Introduction to Computer System Here :"))
+ComputerProgrammingScore = float(input("Put your Computer Programming Score here :"))
+print( "Foundation English :",FoundationEnglishScore)
+print ("General Business :", GeneralBusinessScore)
+print("Introductionto Computer System :",IntroductiontoComputerSystemsScore)
+print ("Computer Programming :",ComputerProgrammingScore)
