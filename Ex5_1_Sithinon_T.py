dd4957f608cc3e2fea70fa9584f0a5a8f86d921c
aneforollll <sithinon2006@gmail.com>
2023-09-08 18:26:50 +0700

    Ex5

---
+++ Ex5_1_Sithinon_T.py
@@ -0,0 +1,10 @@
+a = int(input("first num :"))
+b = int(input("Second num :"))
+plus = a+b
+minus = a-b
+time = a*b
+divide = a/b
+print(a,"+",b,"=",plus)
+print(a,"-",b,"=",minus)
+print(a,"*",b,"=",time)
+print(a,"/",b,"=",divide)
